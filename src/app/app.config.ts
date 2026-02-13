221bbc46d0e74ff1af333f94d0fd0b8e63535f20
Jamez <adobeclip2003@gmail.com>
2026-02-13 12:04:52 +0100

    FEAT: init firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,25 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
+import {FirebaseOptions, initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
 
+// init firebase
+const firebaseConfig: FirebaseOptions = {
+  apiKey: environment.auth.firebase.api_key,
+  appId: environment.auth.firebase.app_id,
+  authDomain: environment.auth.firebase.auth_domain,
+  messagingSenderId: environment.auth.firebase.messaging_sender_id,
+  projectId: environment.auth.firebase.project_id,
+  storageBucket: environment.auth.firebase.storage_bucket,
+  measurementId: environment.auth.firebase.measurement_id
+};
+
+export const firebase_app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(firebase_app);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/services/page-wide/auth/auth-service.ts
+++ src/app/services/page-wide/auth/auth-service.ts
@@ -1,10 +1,16 @@
 import { Injectable, signal } from '@angular/core';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private loading = signal<boolean>(false);
+  auth = getAuth(firebase_app);
 
   getLoading() {
     return this.loading;
@@ -14,4 +20,7 @@ export class AuthService {
     this.loading.set(isLoading);
   }
 
+  async logout() {
+    this.auth.signOut();
+  }
 }
